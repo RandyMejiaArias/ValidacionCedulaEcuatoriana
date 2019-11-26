0e2bf285995a204c000f54e9e41366986cc27241
Randy Mejia <42918668+RandyMejiaArias@users.noreply.github.com>
2019-11-26 02:19:49 -0500

    Create ValidaCedula.java

---
+++ ValidaCedula.java
@@ -0,0 +1,38 @@
+    public boolean verificaCedula(String numeroCedula)
+    {
+        int suma=0;
+        if(numeroCedula.length()==9)
+            return false;
+        else{
+            int a[]=new int [numeroCedula.length()/2];
+            int b[]=new int [(numeroCedula.length()/2)];
+            int c=0;
+            int d=1;
+            for (int i = 0; i < numeroCedula.length()/2; i++) 
+            {
+                a[i]=Integer.parseInt(String.valueOf(numeroCedula.charAt(c)));
+                c=c+2;
+                if (i < (numeroCedula.length()/2)-1) {
+                  b[i]=Integer.parseInt(String.valueOf(numeroCedula.charAt(d)));
+                  d=d+2;
+                }
+            }
+
+            for (int i = 0; i < a.length; i++) 
+            {
+                a[i]=a[i]*2;
+                if (a[i] >9)
+                  a[i]=a[i]-9;
+                suma=suma+a[i]+b[i];
+            } 
+            int aux=suma/10;
+            int dec=(aux+1)*10;
+            if ((dec - suma) == Integer.parseInt(String.valueOf(numeroCedula.charAt(numeroCedula.length()-1))))
+                return true;
+            else
+                if(suma%10==0 && numeroCedula.charAt(numeroCedula.length()-1)=='0')
+                  return true;
+                else
+                  return false;
+        }
+    }
